198bda0373813c9752570ac90cad924f946f77ad
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-08-09 01:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const firstName = document.getElementsByTagName("input")[0]
+const secondName= document.getElementsByTagName("input")[1]
+// console.log(firstName.value);
 function getFormvalue() {
     //Write your code here
+	// console.log(firstName.value);
+   alert(firstName.value+" "+secondName.value);
 
 }
